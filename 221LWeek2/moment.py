d1202de67a824256e51557964a9902ff5654a2dd
LSuro <90302645+LSuro@users.noreply.github.com>
2021-09-08 21:09:33 -0400

    Week 2 Work 221L
    Examples completed in week2 of work in 221L

---
+++ 221LWeek2/moment.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_moment import Moment
+from datetime import datetime
+from bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
